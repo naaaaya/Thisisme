a1f1f40064ba3803121e60d93c7c97a117c20256
naya saito <S-Group@Unendlich-MacBook-Air.local>
2015-08-09 14:14:54 +0900

    Initial Commit

---
+++ koushien/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  koushien
+//
+//  Created by naya saito on 2015/08/09.
+//  Copyright (c) 2015年 naya saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
